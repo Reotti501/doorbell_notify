199927b7d3768b783e4d130c62f1e3fe1664f021
Reotti <leonamiyao119@gmail.com>
2024-11-23 04:26:01 +0900

    LINE Notifyから LINE Massageing APIに変更

--- src/Doorbell_notify.py
+++ src/Doorbell_notify.py
@@ -5,26 +5,26 @@ import pyaudio
 import requests
 import time
 from dotenv import load_dotenv
-from error_hider import noalsaerr  # 第2回参照。ALSA Errorが出ない場合は不要。
+from error_hider import noalsaerr  # ALSA Error が出ない場合は不要
 
 load_dotenv(verbose=True)
 
 Check_every_time = False  # 検知したときにFFTプロット。実際に運用するときはFalse。
 LINE_token = os.getenv('Notify_API_TOKEN')
-RECORD_SECONDS = 1  # 第3回で使用した値に合わせる
-threshold = 1.5e7  # 要調整
+RECORD_SECONDS = 1  # 音声記録時間
+threshold = 1.5e7  # 検知強度閾値（要調整）
 threshold2 = 5
-freq_indices = [797, 798, 799, 800, 801]  # 第3回で決めた値を入れる
+freq_indices = [797, 798, 799, 800, 801]
 freq_indices2 = [f * 2 for f in freq_indices]
-input_device_index = 1  # 第1回で調べた値
-CHUNK = 1024 * 4  # 第1~3回で使用した値に合わせる
+input_device_index = 1  # マイクデバイスのインデックス
+CHUNK = 1024 * 4
 FORMAT = pyaudio.paInt16
 CHANNELS = 1  # モノラル入力
-RATE = 44100  # 第1~3回で使用した値に合わせる
+RATE = 44100
 rng = int(RATE / CHUNK * RECORD_SECONDS)
 
 def setup():
-    with noalsaerr():  # 第2回参照。ALSA Errorが出ない場合はwith文は不要。
+    with noalsaerr():  # ALSA Error 対策
         p = pyaudio.PyAudio()
     stream = p.open(
         format=FORMAT,
@@ -39,7 +39,7 @@ def setup():
 def collect_data(stream, rng, CHUNK):
     frames = []
     for i in range(rng):
-        data = stream.read(CHUNK, exception_on_overflow=False)  # 第2回参照
+        data = stream.read(CHUNK, exception_on_overflow=False)
         frames.append(data)
     d = np.frombuffer(b"".join(frames), dtype="int16")
     return d
@@ -54,33 +54,19 @@ def calc_FFTamp(frames, freq_indices, freq_indices2):
     return amp, amp2
 
 def check_plot(d):
-    fft_data = np.abs(np.fft.fft(d))  # FFTした信号の強度
-    freqList = np.fft.fftfreq(d.shape[0], d=1.0 / RATE)  # 周波数（グラフの横軸）の取得
+    fft_data = np.abs(np.fft.fft(d))
+    freqList = np.fft.fftfreq(d.shape[0], d=1.0 / RATE)
     plt.plot(freqList, fft_data)
-    plt.xlim(0, 5000)  # 0～5000Hzまでとりあえず表示する
+    plt.xlim(0, 5000)
     plt.show()
 
-def send(token, amp, amp2, threshold, threshold2):
-    url = "https://notify-api.line.me/api/notify"
-    token = token
-    headers = {"Authorization": "Bearer " + token}
-    message = (
-        "が鳴ってるよ\n強度 {:.2e} --- 基準 {:.1e}\n比率 {:.2e} --- 基準 {:.1e}".format(
-            amp, threshold, amp / amp2, threshold2
-        )
-    )
-    payload = {"message": message}
-    r = requests.post(url, headers=headers, params=payload)
-
-# Send LINE message
-def line_message():
-    url = "https://api.line.me/v2/bot/message/push"
+# LINE Messaging API を使用して通知を送信する
+def line_message(message):
     access_token = os.getenv('Messaging_API_TOKEN')
     headers = {
         "Authorization": "Bearer " + access_token,
         "Content-Type": "application/json",
     }
-    # Loop through user_ids
     user_ids = os.getenv('USER_IDS').split(',')
     for user_id in user_ids:
         payload = {
@@ -88,10 +74,12 @@ def line_message():
             "messages": [
                 {
                     "type": "text",
-                    "text": os.getenv('MESSAGE'),
+                    "text": message,
                 }
             ]
         }
+        url = "https://api.line.me/v2/bot/message/push"
+        requests.post(url, headers=headers, json=payload)
 
 if __name__ == "__main__":
     p, stream = setup()
@@ -102,7 +90,14 @@ if __name__ == "__main__":
             amp, amp2 = calc_FFTamp(d, freq_indices, freq_indices2)
             if (amp > threshold) & (amp / amp2 > threshold2):
                 print("Someone is at the door.")
-                send(LINE_token, amp, amp2, threshold, threshold2)
+                # メッセージ内容を定義
+                message = (
+                    "が鳴ってるよ\n強度 {:.2e} --- 基準 {:.1e}\n比率 {:.2e} --- 基準 {:.1e}".format(
+                        amp, threshold, amp / amp2, threshold2
+                    )
+                )
+                # LINE メッセージ送信
+                line_message(message)
                 if Check_every_time:
                     check_plot(d)
                 time.sleep(5)
